9e61475425057e9e6c72ecc9cf95bca3ae6b916d
BojanaRatkovic <111780122+BojanaRatkovic@users.noreply.github.com>
2023-01-17 21:27:10 +0100

    Add files via upload

---
+++ 3 - JS  Texts/script.js
@@ -0,0 +1,23 @@
+document.getElementById("showText").addEventListener("click", function () {
+    var firstName, lastName, age, myText, finalText;
+    firstName = document.getElementById("firstName").value;
+    lastName = document.getElementById("lastName").value;
+    age = document.getElementById("age").value;
+    myText = document.getElementById("myText").value;
+    finalText =
+        "Ja, " +
+        firstName +
+        " " +
+        lastName +
+        ", " +
+        age +
+        " godina, " +
+        "izjavljujem sledece: " +
+        myText;
+
+    document.getElementById("result").textContent = finalText;
+
+    var message = "Ova poruka sadrzi " + finalText.length + " karaktera";
+    /*document.write(result.length);*/
+    document.getElementById("message").textContent = message;
+    });
